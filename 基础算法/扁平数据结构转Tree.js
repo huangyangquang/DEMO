5b96e52b7bb5ea3a0cfde664b83da22684d467f9
huangfengquan <572518423@qq.com>
2021-07-31 20:28:53 +0800

    扁平数据结构转TREE

---
+++ 基础算法/扁平数据结构转Tree.js
@@ -0,0 +1,134 @@
+// 扁平数据结构转Tree.js
+
+// 思路：id就是部门的唯一认证，pid就是部门分组的唯一认证。
+
+// 普通版本：
+/**
+ * 递归查找，获取children
+ */
+const getChildren = (data, result, pid) => {
+  for (const item of data) {
+    if (item.pid === pid) {
+      const newItem = {...item, children: []};
+      result.push(newItem);
+      getChildren(data, newItem.children, item.id);
+    }
+  }
+}
+
+/**
+* 转换方法
+*/
+const arrayToTree = (data, pid) => {
+  const result = [];
+  getChildren(data, result, pid)
+  return result;
+}
+
+const arr = [
+    {id: 1, name: '部门1', pid: 0},
+    {id: 2, name: '部门2', pid: 1},
+    {id: 3, name: '部门3', pid: 1},
+    {id: 4, name: '部门4', pid: 3},
+    {id: 5, name: '部门5', pid: 4},
+]
+
+const res = arrayToTree(arr, 0)
+
+console.log(res)
+
+
+// 进阶版本：
+function arrayToTree1(items) {
+  const result = [];   // 存放结果集
+  const itemMap = {};  // 存储中间值
+    
+  // 先转成map存储
+  for (const item of items) {
+    itemMap[item.id] = {...item, children: []}
+  }
+  
+  for (const item of items) {
+    const id = item.id;
+    const pid = item.pid;
+    const treeItem =  itemMap[id];
+    if (pid === 0) {
+      result.push(treeItem);
+    } else {
+      if (!itemMap[pid]) {
+        itemMap[pid] = {
+          children: [],
+        }
+      }
+      itemMap[pid].children.push(treeItem)
+    }
+
+  }
+  return result;
+}
+
+const arr1 = [
+  {id: 1, name: '部门1', pid: 0},
+  {id: 2, name: '部门2', pid: 1},
+  {id: 3, name: '部门3', pid: 1},
+  {id: 4, name: '部门4', pid: 3},
+  {id: 5, name: '部门5', pid: 4},
+]
+
+const res1 = arrayToTree1(arr1)
+console.log(res1)
+
+
+// 高阶版：
+function arrayToTree2(items) {
+  const result = [];   // 存放结果集
+  const itemMap = {};  // 存放中间值
+
+  for (const item of items) {
+    const id = item.id;
+    const pid = item.pid;
+
+    if (!itemMap[id]) {
+      itemMap[id] = {
+        children: [],
+      }
+    }
+
+    itemMap[id] = {
+      ...item,
+      children: itemMap[id]['children']
+    }
+
+    const treeItem =  itemMap[id];
+
+    if (pid === 0) {
+      result.push(treeItem);
+    } else {
+      if (!itemMap[pid]) {
+        itemMap[pid] = {
+          children: [],
+        }
+      }
+      itemMap[pid].children.push(treeItem)
+    }
+
+  }
+  return result;
+}
+
+const arr2 = [
+  {id: 1, name: '部门1', pid: 0},
+  {id: 2, name: '部门2', pid: 1},
+  {id: 3, name: '部门3', pid: 1},
+  {id: 4, name: '部门4', pid: 3},
+  {id: 5, name: '部门5', pid: 4},
+]
+
+const res2 = arrayToTree2(arr2)
+console.log(res2)
+
+
+
+
+// 参考：
+// https://juejin.cn/post/6983904373508145189
